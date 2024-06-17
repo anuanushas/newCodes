bdeecb1866f46cd33a4b5bb18c9b86d72a8ada87
anusha <anu636238@gmail.com>
2024-06-17 18:46:07 +0530

    .\getElement\

---
+++ getElement/index.js
@@ -0,0 +1,14 @@
+let mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+let headerDiv = document.getElementById('header');
+headerDiv.style.backgroundColor = 'green';
+headerDiv.style.borderBottom = '1px solid orange';
+
+let basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+let thanksDiv = document.getElementById('thanks');
+
+thanksDiv.innerHTML = `<p>Please visit us again</p>`
