700b93ba47861589cc5319cdc4648b79e1ed0977
anusha <anu636238@gmail.com>
2024-06-17 21:00:57 +0530

    getTagName

---
+++ getTagName/index.js
@@ -0,0 +1,9 @@
+
+const liElements = document.getElementsByTagName("li");
+liElements[4].style.color = "blue";
+for (let i = 0; i < liElements.length; i++) {
+    liElements[i].style.fontStyle = "italic";
+}
+
+
+
