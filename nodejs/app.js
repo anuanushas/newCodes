246bca1fcff362643937bf6f0623fcbacf9f39b0
anusha <anu636238@gmail.com>
2024-08-24 00:29:48 +0530

    packagejson

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,6 +3,6 @@ const requestHandler = require("./routes");
 
 const server = http.createServer(requestHandler);
 
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+server.listen(4000, () => {
+    console.log("Server is running on http://localhost:4000");
 });
